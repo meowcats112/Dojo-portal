202050c780f34839d0efb30e813f0f42d3b59167
meowcats112 <ash.laretive@gmail.com>
2025-09-25 10:12:27 +1000

    Update app.py

--- dojo_streamlit_portal/app.py
+++ dojo_streamlit_portal/app.py
@@ -250,7 +250,10 @@ if st.session_state.member is not None:
         st.write("")  # spacer
 
     elif nav == "Leave request":
-        st.subheader("Request leave (weekly, starts Monday)")
+        st.subheader("Request Leave")
+        st.write("Members may suspend their membership for 8 weeks per calendar year. The first 4 weeks will be free of charge and the following 4 weeks will be at 
+        a discounted rate of $10 per week.  \nSuspensions must be submitted via the portal with a minimum of 14 days notice. Failure to do so may incur a $10 
+        short-notice fee or result in request being rejected.")
     
         import datetime as _dt
         def next_monday(d: _dt.date) -> _dt.date:
